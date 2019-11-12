5866c4e3b745427d4fe64b13b84d60fb1385df11
Vítor Norberto <vithor.norberto@gmail.com>
2019-11-12 17:50:12 -0300

    Adiciona footer a pagina principal

--- Frontend/angular/src/app/app.module.ts
+++ Frontend/angular/src/app/app.module.ts
@@ -8,18 +8,22 @@ import { TooltipModule } from 'ngx-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SensorComponent } from './componentes/sensor/sensor.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SensorComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
-    TooltipModule.forRoot()
+    TooltipModule.forRoot(),
+    ReactiveFormsModule
   ],
   bootstrap: [AppComponent]
 })
